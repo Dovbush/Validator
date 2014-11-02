5c5412323d17d2f57bcdb1888e5f34a102bf6d83
monsert <andrew.dmn@gmail.com>
2014-11-03 01:06:52 +0200

    add more file

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import put, run
+from fabvenv import Venv
+
+
+ROOT = "/opt/lv128/Validation/"
+
+def deploy():
+    venv = Venv(ROOT, "requirements.txt")
+    if not venv.exists():
+        venv.create()
+    venv.install()
+    put("validation.py", ROOT)
+    put("lv128.service", ROOT)
+    run("sudo mv %s/lv128.service /etc/systemd/system/" % ROOT)
+    run("sudo systemctl enable lv128")
+    run("sudo systemctl restart lv128")
--- validation.py
+++ validation.py
@@ -1,69 +1,73 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import logging
 import pika
 
-valid_mes = "Hello everybody!" 			#here should a reference on a real message 
-queue_valid = 'validation.messages1'    #this is a queue the messages come from
-queue_httpL = 'httpListener'            #this is the next queue the messages go to
-queue_all = 'all.messages1'
-#queue_tocken = 'tocken'.join(id)...
-#id = triplet (the reference on this variable comes from HTTPListener)
-confirmation = 'Response 200 - OK'
-error400 = 'Error 400 - Bad requst'   
-max_length = 128
-mode = 2 
-mes_in_work = 1								#this is an internal rabbitMQ's settings           
+LOG_LOCATION= "yaps-validation.log"
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+
+QUEUE_VALIDATION = "validation.messages"
+QUEUE_HTTPLISTENER = "httplistener"
+QUEUE_MSG_ALL = "message.all"
+RABBITMQ_SERVER = "localhost"
+
+GOOD_MSG = "Response 200 - OK"
+BAD_MSG = "Error 400 - Bad requst"
+MAX_LENGTH = 128
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-              'localhost'))  						
-channel = connection.channel()                      #open connection
-
-def callback(ch, method, properties, body):
-	"""this function consume messages and acknowledge them"""
-	ch.basic_ack(delivery_tag = method.delivery_tag)
-
-def pushing():
-	"""function is cheking for messages length,
-		if True, the message form validation queue is moving to the next queue 
-		where message is ready to be sent to consumer.
-		In other case - it moves to the queue with refused messages"""
-	channel.basic_qos(prefetch_count=mes_in_work)
-	channel.basic_consume(callback,
-               				queue=queue_valid)
-	if len(valid_mes) < max_length:
-		channel.queue_declare(queue=queue_all)
-		channel.basic_publish(exchange='',
-                   				routing_key=queue_all,
-                   				body=valid_mes,
-                   				properties=pika.BasicProperties(
-                  				delivery_mode = mode,                    # make message persistent
-                   				))
-		channel.queue_declare(queue=queue_httpL)
-		channel.basic_publish(exchange='',
-                   				routing_key=queue_httpL,
-                   				body=confirmation,
-                   				properties=pika.BasicProperties(
-                   				delivery_mode = mode,                    # make message persistent
-                   				))
-		logging.basicConfig(format='%(asctime)s %(message)s', filename='info.log', level=logging.INFO)
-		logging.info(confirmation, valid_mes)
-		#we need to add queue - #queue_tocken = 'tocken'.join(id)...
-	else:
-		channel.queue_declare(queue=queue_httpL)
-		channel.basic_publish(exchange='',
-                   				routing_key=queue_httpL,
-                   				body=error400,
-                   				properties=pika.BasicProperties(
-                   				delivery_mode = mode,                    # make message persistent
-                   				))
-		logging.basicConfig(format='%(asctime)s %(message)s', filename='info.log', level=logging.INFO)
-		logging.info(error400)
-			
-pushing()
-channel.start_consuming()
-#connection.close()
+class Validation():
+	"""
+	the message form validation queue is moving to the next queue 
+	where message is ready to be sent to consumer.
+	In other case - it moves to the queue with refused messages
+	"""
+	def __init__(self):
+		self.log = logging.getLogger(LOG_LOCATION)
+		self.log.setLevel(logging.INFO)
+		log_hand = logging.FileHandler(LOG_LOCATION)
+		log_hand.setLevel(logging.INFO)
+		formatter = logging.Formatter(LOG_FORMAT)
+		log_hand.setFormatter(formatter)
+		self.log.addHandler(log_hand)
 
 
+		self.connection = pika.BlockingConnection(
+			pika.ConnectionParameters(RABBITMQ_SERVER))
+		
+		if self.connection.is_open:
+			self.log.info("connected to rabbitmq")
+		else:
+			self.log.error("no connection to rabbitmq")
+
+	def valid(self):
+		tmp_msg = self.get_msg(QUEUE_VALIDATION)
+		print tmp_msg
+		if len(tmp_msg) < MAX_LENGTH:
+			self.log.info(GOOD_MSG, tmp_msg)
+			self.send_msg(QUEUE_MSG_ALL, tmp_msg)
+			self.send_msg(QUEUE_HTTPLISTENER, GOOD_MSG)
+		else:
+			self.send_msg(QUEUE_HTTPLISTENER, BAD_MSG)
+			self.log.error(BAD_MSG)
+		
+	def get_msg(self, my_queue):
+		channel = self.connection.channel()
+		for method_frame, properties, body in channel.consume(my_queue):
+			self.log.info(body)
+			channel.basic_ack(method_frame.delivery_tag)
+			channel.close_channel()
+			return body
+
+	def send_msg(self, my_queue, msg_body):
+		channel = self.connection.channel()
+		channel.queue_declare(queue=my_queue)
+		channel.basic_publish(exchange='', routing_key=my_queue, body=msg_body)
+
+
+if __name__ == '__main__':
+	v = Validation()
+	for x in xrange(5):
+		print x
+		v.valid()
+	
+	

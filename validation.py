fdceacf809665be4e9b8c0726096fc47b55a10e2
Andrew <monsert@users.noreply.github.com>
2014-11-14 19:54:36 +0200

    Update validation.py

--- validation.py
+++ validation.py
@@ -2,7 +2,7 @@
 import logging
 import pika
 
-LOG_LOCATION= "yaps-validation.log"
+LOG_LOCATION= "/opt/lv128/Validation/yaps-validation.log"
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 
 QUEUE_VALIDATION = "validation.messages"

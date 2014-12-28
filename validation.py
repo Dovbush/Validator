b3141ccd182ad1e15325f79bffc23b7e116acbd0
imax88 <imax88@users.noreply.github.com>
2014-12-28 15:08:07 +0200

    reviewed code final

--- validation.py
+++ validation.py
@@ -1,22 +1,21 @@
-# -*- coding: utf-8 -*-
-
-import logging
-
-import sqlite3 # or mysql connector
-
 import pika
-from time import sleep
-
+import logging
 import mysql.connector
+from types import *
+from time import sleep
 
 LOG_LOCATION= "/opt/lv128/log/validation.log"
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-
 QUEUE_VALIDATION = "validation.messages"
 QUEUE_HTTPLISTENER = "httplistener"
 QUEUE_MSG_ALL = "message.all"
 QUEUE_UUID = "message."
-RABBITMQ_SERVER = "localhost"
+
+RABBITMQ_SERVER = "127.0.0.1"
+RABBITMQ_USER = "lv128"
+RABBITMQ_PASSWORD = "lv128"
+RABBITMQ_PORT = 5672
+
 CONNECT_ON = "connected to rabbitmq"
 CONNECT_OFF = "no connection to rabbitmq"
 SQL_CONNECT_ON = "connected to sql"
@@ -24,14 +23,21 @@ SQL_CONNECT_OFF = "no connection to sql"
 EMPTY = "can't consume - queue is empty"
 MAX_LENGTH = 128
 MAX_NUMBER_FIELD = 3
-GOOD_MSG = "Response 200 - OK"
-BAD_LENGTH = "Error 400 - Bad requst, message is longer than %s" % MAX_LENGTH
-INVALID_TOKEN = "Error 406 - Invalid token."
-MISSING_ELEMENTS = "Error 400 - Bad requst, hex, token or message are missing"
-GET_USER_COUNTERS = "SELECT total_msg_counter, success_msg_counter, failed_msg_counter from my_app_msg WHERE user_id={0}"
-UPDATE_USER_COUNTERS = "UPDATE my_app_msg SET total_msg_counter={}, success_msg_counter={}, failed_msg_counter={}  WHERE user_id={}"
-FIND_PROFILE_BY_TOKEN = "SELECT * FROM my_app_profile WHERE token={}"
 
+DB_SERVER = "127.0.0.1"
+DB_USER = "root"
+DB_PASSWORD = ''
+DB_NAME = 'yaps'
+GOOD_MSG = "200|Response 200 - OK."
+BAD_LENGTH = "400|Error 400 - Bad requst, message is longer than %s" % MAX_LENGTH
+INVALID_TOKEN = "406|Error 406 - Invalid token."
+MISSING_ELEMENTS = "400|Error 400 - Bad requst, hex, token or message are missing"
+
+GET_USER_COUNTERS = "SELECT total_msg_counter, success_msg_counter, failed_msg_counter from my_app_msg WHERE user_id={}"
+UPDATE_USER_COUNTERS = "UPDATE my_app_msg SET total_msg_counter={}, success_msg_counter = {}, failed_msg_counter = {} WHERE user_id ={}"
+FIND_PROFILE_BY_TOKEN = "SELECT * FROM my_app_profile WHERE token='{}'"
+SELECT_COUNTERS_BY_ID = "SELECT total_msg_counter, success_msg_counter, failed_msg_counter FROM my_app_msg WHERE user_id={}"
+SELECT_USER_BY_TOKEN = "SELECT * FROM my_app_profile WHERE token = '{}'"
 class Validation():
     """
     the message form validation queue is moving to the next queue
@@ -40,13 +46,11 @@ class Validation():
     """
 
     def __init__(self):
-       # credentials = pika.PlainCredentials('guest', 'guest')
-        credentials = pika.PlainCredentials('lv128', 'lv128')
-        parameters = pika.ConnectionParameters('localhost',
-                                       5672,
+        credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+        parameters = pika.ConnectionParameters(RABBITMQ_SERVER,
+                                       RABBITMQ_PORT,
                                        '/',
                                        credentials)
-
         self.log = logging.getLogger(LOG_LOCATION)
         self.log.setLevel(logging.INFO)
         log_hand = logging.FileHandler(LOG_LOCATION)
@@ -55,11 +59,10 @@ class Validation():
         log_hand.setFormatter(formatter)
         self.log.addHandler(log_hand)
         try:
-            #self.sql_conn = sqlite3.connect('users.db') # or mysql db connect
-            #mysql DB
-            self.sql_conn =mysql.connector.connect(user='root', password='',
-                              host='127.0.0.1',
-                              database='yaps')
+
+            self.sql_conn = mysql.connector.connect(user=DB_USER, password=DB_PASSWORD,
+                              host=DB_SERVER,
+                              database=DB_NAME)
 
             self.sql_cursor = self.sql_conn.cursor()
             self.log.info(SQL_CONNECT_ON)
@@ -75,18 +78,29 @@ class Validation():
             raise
 
     def update_user_counters(self, record, is_valid):
-        is_valid = 1 if is_valid else 0
-
+        id = record[0]
+        token = record[1]
+        user_id = record[2]
+        self.sql_cursor.execute(SELECT_COUNTERS_BY_ID.format(user_id))
+        result = self.sql_cursor.fetchone()
+        total = result[0] + 1
+        success = result[1]
+        fail = result[2]
+        if is_valid:
+            success = success + 1
+        else:
+            fail = fail + 1
+        self.sql_cursor.execute(UPDATE_USER_COUNTERS.format(str(total), str(success), str(fail), str(user_id)))
+        self.sql_conn.commit()
         id, token, user_id = record
-        self.sql_cursor.execute(GET_USER_COUNTERS.format(user_id))
+        self.sql_cursor.execute(GET_USER_COUNTERS.format(str(user_id)))
         total, success, fail = self.sql_cursor.fetchone()
-        self.sql_cursor.execute(UPDATE_USER_COUNTERS.format(total + 1, success + int(is_valid), fail + int(not is_valid),  user_id))
-    
+        self.sql_cursor.execute(UPDATE_USER_COUNTERS.format(str(total), str(success), str(fail), str(user_id)))
+
     def get_valid_record(self, token):
-        self.sql_cursor.execute(FIND_PROFILE_BY_TOKEN.format(token))
-        for record in self.sql_cursor.fetchall():
-            # return first match
-            return record
+        self.sql_cursor.execute(SELECT_USER_BY_TOKEN.format(str(token)))
+        result = self.sql_cursor.fetchone()
+        return result
 
     def valid(self):
         """Every messages must have 3 elements: hex, token and message
@@ -97,8 +111,8 @@ class Validation():
         queue_uuid = test_msg[1]
         my_message = test_msg[2]
         valid_record = self.get_valid_record(queue_uuid)
-        if len(test_msg) == MAX_NUMBER_FIELD and len(my_message) < MAX_LENGTH and valid_record:
-                self.update_user_counters(valid_record)
+        if len(test_msg) == MAX_NUMBER_FIELD and len(my_message) < MAX_LENGTH and not  (type(valid_record) is NoneType):
+                self.update_user_counters(valid_record, 1)
                 self.log.info(GOOD_MSG + " " + my_message)
                 self.send_msg(QUEUE_MSG_ALL, my_message)
                 self.send_msg((QUEUE_UUID + queue_uuid), my_message)
@@ -107,15 +121,14 @@ class Validation():
             if len(test_msg) != MAX_NUMBER_FIELD :
                 self.send_msg(QUEUE_HTTPLISTENER, MISSING_ELEMENTS)
                 self.log.error(MISSING_ELEMENTS)
-            elif len(my_message) < MAX_LENGTH :
-                if valid_record:
-                    # token is valid but message could not be sent
-                    self.update_user_counters(valid_record)
+                self.update_user_counters(valid_record, False)
+            elif len(my_message) > MAX_LENGTH :
                 self.send_msg(QUEUE_HTTPLISTENER, BAD_LENGTH)
                 self.log.error(BAD_LENGTH)
+                self.update_user_counters(valid_record, False)
             else:
                 self.log.error(INVALID_TOKEN)
-
+                self.send_msg(QUEUE_HTTPLISTENER, INVALID_TOKEN)
 
     def get_msg(self, my_queue):
         """The function takes message from the queue"""
@@ -143,4 +156,3 @@ if __name__ == '__main__':
     while True:
         v.valid()
         sleep(0.05)
-
